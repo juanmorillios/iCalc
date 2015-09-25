c776f9c98dca663e98104132ea4f66b1633f3431
Juan Ml. Morillo <juanmorillios@gmail.com>
2015-09-25 18:41:00 +0200

    Calc. Basic
    Calc. Basic + UI + Switch + Layout

--- iCalc/ViewController.swift
+++ iCalc/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblResult: UILabel!
+    
+    //Declaramos la variable que va a guardar el resultado:
+    
+    var result: Float = 0
+    
+    //Declaramos la variable que va a mantener el último numero pulsado
+    
+    var lastNumPress: Float = 0
+    
+    //Declaramos la variable que va a mantener la última operación seleccionada.
+    
+    var lastPressOpt: String = "="
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
+    //Asignamos el valor de la variable result al Label, el tipo de dato de la variable result es Float, para poder mostrarlo en la propiedad Text del label hay que convertirlo a  tipo String, a continuación.
+        
+        
+     lblResult.text = "\(result)"
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +41,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pressNum(sender: UIButton) {
+        
+        
+        
+        lastNumPress = lastNumPress * 10 + Float(Int(sender.titleLabel!.text!)!)
+        
+        lblResult.text = "\(lastNumPress)"
+        
+    }
 
+    @IBAction func pressOpt(sender: UIButton) {
+    }
+ 
+    @IBAction func clearBtn(sender: UIButton) {
+    }
 }
 
